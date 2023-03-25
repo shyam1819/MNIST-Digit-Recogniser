e6cc4a6e1d6239dab42e649409239acf101f3ae4
shyam1819 <81102114+shyam1819@users.noreply.github.com>
2023-03-26 00:24:09 +0530

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from PIL import Image
+#from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import tensorflow as tf
